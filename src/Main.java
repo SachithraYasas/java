acb4e1caea9cf5e9e9e8d583edfaa4580026b24e
Sachithra Yasas <sachithra.ekanayaka@gmail.com>
2023-10-18 10:54:21 +0530

    Java project sample

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    Main(){
+        String name= "Programming Language";
+        System.out.println(name + " Java");
+
+    }
+    public static void main(String[] args) {
+
+        Main m = new Main();
+    }
+}
